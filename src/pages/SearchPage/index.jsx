621c91303448e8490d09b7389920c32ab3792470
신희성 <vvwv3092@gmail.com>
2023-11-20 17:53:08 +0900

    Add SearchPage style

--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -1,5 +1,9 @@
 import * as S from "./style";
 import Header from "../../components/Header";
+import { SearchIcon } from "../../assets/SearchIcon";
+import { InfoUserIcon } from "../../assets/InfoUserIcon";
+import { ProfileIcon } from "../../assets/ProfileIcon";
+import { InfoButton } from "../../assets/InfoButton";
 
 export default function SearchPage() {
   return (
@@ -7,10 +11,19 @@ export default function SearchPage() {
       <Header />
       <S.Container>
         <S.SearchContainer>
-          <S.SearchInput />
+          <S.SearchBox>
+            <S.SearchInput />
+            <S.SearchIcon>
+              <SearchIcon />
+            </S.SearchIcon>
+          </S.SearchBox>
           <S.UserContainer>
             <S.UserBox>
-              <S.UserName>이진헌</S.UserName>
+              <S.UserInfoBox>
+                <ProfileIcon />
+                <S.UserName>이진헌</S.UserName>
+              </S.UserInfoBox>
+              <InfoButton />
             </S.UserBox>
           </S.UserContainer>
         </S.SearchContainer>
--- src/pages/SearchPage/style.js
+++ src/pages/SearchPage/style.js
@@ -10,20 +10,64 @@ export const Container = styled.div`
 `;
 
 export const SearchContainer = styled.div`
-  width: 70%;
-  height: 90%;
+  width: 550px;
+  height: 70%;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   border: 1px solid;
+  gap: 50px;
 `;
 
-export const SearchInput = styled.input``;
+export const SearchInput = styled.input`
+  width: 100%;
+  border-radius: 25px;
+  background: #17161f;
+  outline: none;
+  border: none;
+  color: white;
+  padding: 30px 30px 30px 20px;
+  font-size: 30px;
+  font-weight: 700;
+`;
 
-export const UserContainer = styled.div``;
+export const SearchBox = styled.div`
+  position: relative;
+  width: 100%;
+`;
 
-export const UserBox = styled.div``;
+export const SearchIcon = styled.p`
+  position: absolute;
+  right: 0;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`;
 
-export const UserInfoBox = styled.div``;
-export const UserName = styled.div``;
+export const UserContainer = styled.div`
+  background: #17161f;
+  height: 100%;
+  width: 100%;
+  border-radius: 25px;
+  padding: 20px;
+`;
+
+export const UserBox = styled.div`
+  width: 100%;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: white;
+`;
+
+export const UserInfoBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+
+export const UserName = styled.div`
+  font-size: 22px;
+  font-weight: 700;
+`;
