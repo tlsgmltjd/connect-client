9aa40a8f02ee9dd7bf7f0beb3fb537db3ddd1901
신희성 <vvwv3092@gmail.com>
2023-12-09 15:43:18 +0900

    Update profile page style

--- src/pages/ProfilePage/index.jsx
+++ src/pages/ProfilePage/index.jsx
@@ -1,3 +1,4 @@
+import { UserIcon } from "../../assets/UserIcon";
 import Header from "../../components/Header";
 import * as S from "./style";
 
@@ -5,7 +6,16 @@ export default function ProfilePage() {
   return (
     <S.Container>
       <Header />
-      <S.ProfileContainer></S.ProfileContainer>
+      <S.ProfileContainer>
+        <S.ProfileBox>
+          <UserIcon />
+          <S.ProfileName>신희성</S.ProfileName>
+          <S.ProfileFollowBox>
+            <S.ProfileFollowItem>20 followers</S.ProfileFollowItem>
+            <S.ProfileFollowItem>20 followers</S.ProfileFollowItem>
+          </S.ProfileFollowBox>
+        </S.ProfileBox>
+      </S.ProfileContainer>
     </S.Container>
   );
 }
--- src/pages/ProfilePage/style.js
+++ src/pages/ProfilePage/style.js
@@ -18,3 +18,30 @@ export const ProfileContainer = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
+export const ProfileBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+`;
+
+export const ProfileName = styled.h1`
+  text-shadow: 0 0 0px #fff, 0 0 5px #fff, 0 0 0px #fff;
+  font-size: 30px;
+  color: white;
+  font-weight: 600;
+`;
+
+export const ProfileFollowBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  font-size: 20px;
+  color: white;
+  font-weight: 200;
+`;
+
+export const ProfileFollowItem = styled.div``;
